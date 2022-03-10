bcfe64482d93e1a7611640824b1b72ff28de49e2
konkos <kon_kostas@windowslive.com>
2022-03-11 00:01:31 +0200

    add configuration

---
+++ config/config.py
@@ -0,0 +1,8 @@
+### config.py ###
+from config import local_config
+
+USERNAME = local_config.USERNAME
+PASSWORD = local_config.PASSWORD
+DB_NAME = local_config.DB_NAME
+DATABASE_URI = local_config.DATABASE_URI
+
