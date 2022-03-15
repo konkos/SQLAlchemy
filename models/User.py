df1334fc3c4bf6458d8a7fef31c7809d82859927
konkos <kon_kostas@windowslive.com>
2022-03-15 23:51:19 +0200

    add user Class

--- models/User.py
+++ models/User.py
@@ -2,7 +2,14 @@ from sqlalchemy import Table, Column, Integer, ForeignKey
 
 from models.models import Base
 
-User = Table('user',
-             Base.metadata,
-             Column('pk', Integer(), primary_key=True),
-             Column('author_id', ForeignKey('author.pk'), unique=True))
+user_table = Table('user',
+                   Base.metadata,
+                   Column('pk', Integer(), primary_key=True),
+                   Column('author_id', ForeignKey('author.pk'), unique=True))
+
+
+class User(Base):
+    __table__ = user_table
+
+    def __repr__(self):
+        return f"PK:{self.pk}, AuthorID:{self.author_id}"
